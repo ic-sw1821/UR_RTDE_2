40c1d30146e98fd3e75a711e4d6f02bfd673ce7e
Chaoran S Wang <steven.wang21@imperial.ac.uk>
2024-03-19 15:23:25 +0000

    big commit

--- min_jerk_servoj.py
+++ min_jerk_servoj.py
@@ -9,21 +9,21 @@ import time
 from matplotlib import pyplot as plt
 
 # functions for writing set-points to the robot
-# converts the .xml setp format sent and converts it back into Python form
+    # converts the .xml setp format sent and converts it back into Python form
 def setp_to_list(setp):
     temp = []
     for i in range(0, 6):
         temp.append(setp.__dict__["input_double_register_%i" % i])
     return temp
 
-# writes Python array-form coordinates into .xml setp format to be sent to robot with con.send(setp)
+    # writes Python array-form coordinates into .xml setp format to be sent to robot with con.send(setp)
 def list_to_setp(setp, list):
     for i in range(0, 6):
         setp.__dict__["input_double_register_%i" % i] = list[i]
     return setp
 
 # establish TCP/IP connection with robot
-ROBOT_HOST = '192.168.1.30'
+ROBOT_HOST = '192.168.1.10'
 ROBOT_PORT = 30004
 logging.getLogger().setLevel(logging.INFO) # generates log of how the robot did for debugging
 con = rtde.RTDE(ROBOT_HOST, ROBOT_PORT)
@@ -62,7 +62,6 @@ if not con.send_start():
     # first 3 values are xyz, last 3 are orientations
 start_pose = [0.62899, -0.03993, 0.08944, 3.1415, 0.0001, 0.0001] # scratch 1
 desired_pose = [0.69436, 0.17108, -0.08862, 3.1416, 0.0000, 0.0002] # scratch 2
-scratch_3 = [0.69436, 0.17108, -0.08862, 3.1416, 0.0000, 0.0002]
 
 # waits for user to ensure both .urp and .py files are running before exchanging data
 while True:
@@ -74,26 +73,83 @@ while True:
         print('Boolean 1 is True, Robot Program can proceed\n')
         break
 
-# sets mode 1 and sends coordinates for movej
-watchdog.input_int_register_0 = 1
-con.send(watchdog) # sending mode == 1
-list_to_setp(setp, start_pose)  # store start_pose in setp format
-con.send(setp) # pass setp to .urp program to execute on robot
+length = 0.026 # total length of all blanks to be tested (in m)
+scratched_length = 0 # total length of metal scratched (in m)
 
-    # waits for movej to finish
-while True:
-    print('Waiting for movej() to finish')
+# initial grinding (mode 1)
+watchdog.input_int_register_0 = 1
+con.send(watchdog)
+while True: # Waiting for move to finish
     state = con.receive()
     con.send(watchdog)
     if state.output_bit_registers0_to_31 == False:
-        print('Proceeding to mode 2\n')
+        print('Proceeding\n')
         break
 
-#state = con.receive() # asks to read outputs
-#tcp1 = state.actual_TCP_pose # specifies specific output
+# main infinite operations loop
+while True: # Waiting for move to finish
+    # Dipping (mode 2)
+    watchdog.input_int_register_0 = 2
+    con.send(watchdog)
+    while True:
+        state = con.receive()
+        con.send(watchdog)
 
-# sets mode 3 and ends connection
-watchdog.input_int_register_0 = 3
+        if state.output_bit_registers32_to_63 == False:
+            # valve.powder()
+            print('Powdering\n')
+
+        if state.output_bit_registers0_to_31 == False:
+            print('Proceeding\n')
+            break
+
+    # Scratching (mode 3)
+    i = 0
+    while i<3:
+        i = i + 1
+
+        # valve.feed()
+        time.sleep(0.3)
+
+        watchdog.input_int_register_0 = 3
+        con.send(watchdog)
+
+        # valve.spray(1)
+
+        while True:  # Waiting for 1 scratch to finish
+            state = con.receive()
+            con.send(watchdog)
+
+            if state.actual_TCP_pose[2] > 0.3:
+                # valve.spray(0)
+                print('Spray off\n')
+
+            if state.output_bit_registers0_to_31 == False:
+                # valve.spray(0)
+                scratched_length = scratched_length + 0.002  # increments fed amount
+                print('Proceeding\n')
+                break
+        if scratched_length >= (length-0.02):
+            break
+    if scratched_length >= (length-0.02):
+        break
+
+# initial grinding (mode 1)
+watchdog.input_int_register_0 = 1
+con.send(watchdog)
+while True: # Waiting for move to finish
+    state = con.receive()
+    con.send(watchdog)
+    if state.output_bit_registers0_to_31 == False:
+        print('Proceeding\n')
+        break
+
+# mode 4 - end of operation
+watchdog.input_int_register_0 = 4
 con.send(watchdog)
 con.send_pause() # pause sending the values
 con.disconnect() # disconnect from UR10
+
+# list_to_setp(setp, start_pose)  # store start_pose in setp format
+# con.send(setp) # pass setp to .urp program to execute on robot
+
