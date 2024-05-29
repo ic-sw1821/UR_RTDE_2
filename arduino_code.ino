709062b17c109dd76f749a8d7d6c183a04e2d5ab
WTaylor332 <taylornawalter@outlook.com>
2024-05-29 08:17:43 +0100

    arduino ide change

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,14 +1,10 @@
 int stripFeeder = 2;
-int sprayer = 4;
-int powder = 6;
-int signalPin = 8;
+int sprayer = 7;
 
 void setup() {
   Serial.begin(9600);
   pinMode(stripFeeder, OUTPUT);
   pinMode(sprayer, OUTPUT);
-  pinMode(signalPin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
@@ -17,41 +13,19 @@ void loop() {
 
     // strip feeder solenoid valve
     if (msg == "1"){
-      // digitalWrite(stripFeeder, HIGH)
-      digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(stripFeeder, HIGH);
       delay(1000);
-      // digitalWrite(stripFeeder, LOW)
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(1000); 
+      digitalWrite(stripFeeder, LOW);
+      delay(1000);
+  
     }
     // sprayer 1 solenoid valve
     else if (msg == "3"){
-      // digitalWrite(sprayer, HIGH);
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(2000);
-    }
-    else if (msg == "4"){
-      // digitalWrite(sprayer, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(2000);
-    }
-    // sprayer 2 powder solenoid valve
-    else if (msg == "5"){
-      // digitalWrite(powder, HIGH)
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(3000);
-      // digitalWrite(powder, LOW)
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(3000);
-    }
-    else {
-      for (int i = 0; i < 12; i++){
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-      }
+      digitalWrite(sprayer, HIGH);
+      // digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+      digitalWrite(sprayer, LOW);
+      delay(1000);
     }
   }
 }
-
